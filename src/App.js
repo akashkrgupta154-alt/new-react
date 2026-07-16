5d13d0c54f0810ab1e9a0345c93f8d392f377dcf
Akash Gupta <akashkrgupta154@gmail.com>
2026-07-16 10:41:44 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 class App extends Component{
    render(){
     return(
-        <div><h1>Hello, React !!</h1></div>
+        <div><h1>Hello, React my home !!</h1></div>
     );
    }
 }
