4274a56429acdc48774cce342ff14b64499c4943
Akash Gupta <akashkrgupta154@gmail.com>
2026-07-16 17:28:47 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "./App.css";
 class App extends Component{
    render(){
     return(
-        <div><h1>Hello, React my home !!</h1></div>
+        <div>
+            <h1>Hello, React my home !!</h1>
+            <p>This is my second github command</p>
+        </div>
     );
    }
 }
